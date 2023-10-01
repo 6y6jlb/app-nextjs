65f11812cdf271f84c9ba9aae3610a7234d11cee
AlexB <6y6jlb@gmail.com>
2023-10-01 12:57:42 +0400

    icons and styles refactoring

--- components/projectItem/ProjectItem.tsx
+++ components/projectItem/ProjectItem.tsx
@@ -6,16 +6,19 @@ import { useTranslations } from "next-intl";
 
 
 
-export default function ProjectItem( {activeRepoName, project, setActiveRepoName }: IProps) {
+export default function ProjectItem({ activeRepoName, project, setActiveRepoName }: IProps) {
 	const t = useTranslations("common");
 	const activeStyle = activeRepoName === project.name
 
 	return (
 		<div onMouseEnter={() => setActiveRepoName(project.name)} className={style.project}>
-			<a href={project.html_url} className={style.button}>
-				{t('projects.link-to')}
-			</a>
-			<div className={`${style.image} ${activeStyle ? style.active : ''} ${getImageClass(style, project.name)}`}></div>
+			<div className={style.imageBlock}>
+				<div className={`${style.image} ${activeStyle ? style.active : ''} ${getImageClass(style, project.name)}`} />
+				<a href={project.html_url} className={style.button}>
+					{t('projects.link-to')}
+				</a>
+			</div>
+
 			<div className={style.descriptionBlock}>
 				<h4 className={style.projectName}>{project.name}</h4>
 				<p style={style.description as any}>{project.description}</p>
