1015034745d3170a5e2cad1900aa3db96c5278ad
AlexB <6y6jlb@gmail.com>
2023-11-14 22:38:22 +0400

    a bit animation

--- components/main/Main.tsx
+++ components/main/Main.tsx
@@ -1,15 +1,17 @@
-'use'
-
+import { useTranslations } from "next-intl";
 import style from "./styles.module.css"
 
 const Main = () => {
-
+	const t = useTranslations("common");
+	
 
 	return (
 		<div id={"common"} className={style.main}>
 			<div className={`main-container ${style.container}`}>
+				<p className={style.description}>{t('main.description')}</p>
+				<p className={style.typing}>{t('typing.i')}&nbsp;{t('typing.frontend')}/{t('typing.backend')}/{t('typing.fullstack')}&nbsp;{t('typing.developer')}</p>
+				<div className={style.block}></div>
 			</div>
-
 		</div>
 	)
 }
