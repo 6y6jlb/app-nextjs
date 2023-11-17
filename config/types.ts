6507e696d66054b153e3a6a46f6278cc141ce30e
AlexB <6y6jlb@gmail.com>
2023-11-17 21:22:25 +0400

    routes refactoring/in progress

---
+++ config/types.ts
@@ -0,0 +1,11 @@
+import { LINK_TYPE_ENUM, PATH_TYPE_ENUM } from "./navigation";
+
+export interface ILink {
+    path: string,
+    type: PATH_TYPE_ENUM,
+    title: string
+}
+
+export type ILinks = {
+    [K in LINK_TYPE_ENUM]?: ILink[];
+  };
