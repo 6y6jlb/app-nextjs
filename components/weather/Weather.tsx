07d723749ed2dcaaf916837336a0d34c74620d6b
AlexB <6y6jlb@gmail.com>
2023-11-17 17:48:52 +0400

    weather items styles

--- components/weather/Weather.tsx
+++ components/weather/Weather.tsx
@@ -31,21 +31,22 @@ const Weather = () => {
   const mappedForecasts = React.useMemo(() => forecasts.map(item => {
     const sign = TEMPERATURE_SIGN[item.units];
     return (
-        <div key={String(item.dt)} className="list-item">
-            <p> {t('weather.place')}: {item.name} </p>
-            <p> {t('weather.temperature')}: {String(item.main.temp)} {sign}</p>
-            <p> {t('weather.feels-like')}: {String(item.main.feels_like)} {sign}</p>
-            <p> {t('weather.humidity')}: {String(item.main.humidity)} </p>
-        </div>
+      <div key={String(item.dt)} className={styles.forecast}>
+        <p><span>{t('weather.place')}:</span> <span>{item.name} </span></p>
+        <p><span>{t('weather.temperature')}:</span> <span>{String(item.main.temp)} {sign}</span></p>
+        <p><span>{t('weather.feels-like')}:</span> <span>{String(item.main.feels_like)} {sign}</span></p>
+        <p><span>{t('weather.humidity')}:</span> <span>{String(item.main.humidity)} </span></p>
+      </div>
     )
-}), [forecasts]);
+  }), [forecasts]);
 
 
   return (
     <div className={styles.container}>
+      <p>{t('weather.description')}</p>
       <WeatherForm onSubmit={onSubmit} />
       <div className={styles.forecasts}>
-      {mappedForecasts}
+        {mappedForecasts}
       </div>
     </div>
   )
