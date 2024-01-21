fb12ab58f4db5f08970bb4b86554060bbd2302ad
AlexB <6y6jlb@gmail.com>
2024-01-21 10:49:02 +0300

    tasks view changes

--- modules/sneaky/tasks/storeItemButton/StoreItemButton.tsx
+++ modules/sneaky/tasks/storeItemButton/StoreItemButton.tsx
@@ -1,17 +1,20 @@
 import { useTranslations } from 'next-intl';
 import React from 'react'
+import styles from './styles.module.css'
 
 const StoreItemButton = ({ notCreatedYet: notCreateYet = false, hasTelegramId }: Props) => {
     const t = useTranslations("common");
     return (
-        <div>
+        <div className={styles.container}>
             <p>{t('tasks.create-description')}</p>
             <button disabled={!hasTelegramId} className="btn-primary" onClick={(e) => {
                 location.assign('/tasks/create')
             }}>
                 {t(notCreateYet ? 'button.task-create-first' : 'button.task-create-another')}
             </button>
-            (!hasTelegramId && <p className="alert alert-warning">{t('alert.telegram-does-not-exist')}</p>)
+            {
+                !hasTelegramId && <p className="alert alert-warning">{t('alert.telegram-does-not-exist')}</p>
+            }
         </div>
     )
 }
