aee711577f67b38e889b04abd05e5d43a90f8247
AlexB <6y6jlb@gmail.com>
2024-03-17 14:01:39 +0400

    threejs milestone, anima

--- modules/sneaky/sandbox/threejs/ThreeScene.tsx
+++ modules/sneaky/sandbox/threejs/ThreeScene.tsx
@@ -9,27 +9,62 @@ const ThreeScene = (props: Props) => {
     const containerRef = useRef<HTMLDivElement>(null);
     useEffect(() => {
         const SIZES = {
-            WIDTH: window.innerWidth * 0.9, HEIGHT: window.innerHeight * 0.9
+            WIDTH: window.innerWidth * 0.9, HEIGHT: window.innerHeight * 0.9, MAX_SCALE: 2, MIN_SCALE: 0.1
         }
+        const COLORS = ['#6DE5CC', '#ECEE60', '#B08AC0']
+        const STATE = { GROW: false };
+
         const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(75, SIZES.WIDTH / SIZES.HEIGHT, 0.1, 1000);
+        const camera = new THREE.PerspectiveCamera(75, SIZES.WIDTH / SIZES.HEIGHT);
         const renderer = new THREE.WebGLRenderer();
-        const geometry = new THREE.BoxGeometry();
-        const material = new THREE.MeshBasicMaterial({ color: 'purple', wireframe: true });
-        const cube = new THREE.Mesh(geometry, material);
-        const helper = new THREE.AxesHelper(3)
+        const geometry = new THREE.BoxGeometry(1, 1, 1);
         const clock = new THREE.Clock()
 
-        scene.add(cube);
-        scene.add(helper);
+        const meshes: THREE.Mesh[] = [];
+        const meshesGroup = new THREE.Group()
+
+        for (let x = -2; x <= 2; x = x + 2) {
+            for (let y = -2; y <= 2; y = y + 2) {
+                const material = new THREE.MeshBasicMaterial({ color: COLORS[(Math.random() * 3) | 0], wireframe: true });
+                const mesh = new THREE.Mesh(geometry, material)
+                mesh.position.set(x, y, 0)
+                meshes.push(mesh)
+            }
+
+        }
+
+        meshesGroup.add(...meshes)
+        scene.add(meshesGroup);
         renderer.setSize(SIZES.WIDTH, SIZES.HEIGHT);
         camera.position.z = 5;
 
         const tick = () => {
-            const elapsedTime = clock.getElapsedTime()
-            camera.position.x = Math.sin(elapsedTime);
-            camera.position.y = Math.cos(elapsedTime);
-            camera.lookAt(cube.position)
+            // const elapsedTime = clock.getElapsedTime()
+            // camera.position.x = Math.sin(elapsedTime);
+            // camera.position.y = Math.cos(elapsedTime);
+            camera.lookAt(meshesGroup.position)
+
+
+            const delta = clock.getDelta()
+            meshes.forEach((el, i) => {
+                const multiplier = 1 % 2 ? -1 : 1;
+                el.rotation.x += delta * multiplier;
+                el.rotation.y += delta * multiplier;
+            })
+            const multiplier = STATE.GROW ? 1 : -1;
+
+            if (meshesGroup.scale.x >= SIZES.MAX_SCALE) {
+                STATE.GROW = false
+            } else if (meshesGroup.scale.x <= SIZES.MIN_SCALE) {
+                STATE.GROW = true
+            }
+
+            const newScaleValue = delta * multiplier * 0.3;
+
+            meshesGroup.scale.x += newScaleValue
+            meshesGroup.scale.y += newScaleValue
+            meshesGroup.scale.z += newScaleValue
+
 
             renderer.render(scene, camera);
             requestAnimationFrame(tick);
